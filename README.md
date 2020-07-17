eaa57aef8bb4fe9b9dace986bc3bfc2c684670ab
Russell Chan <russellcxl@gmail.com>
2020-07-17 13:44:36 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Haste
+## Streamlining the order/delivery process for your company's needs
+
+This is will be a delivery system that processes orders and maps out the most efficient delivery path for the day. 
+
+### Motivation
+Some friends own a small business selling gaming chairs. However, because there is no proper order/delivery system, they do things very manually (and inefficiently) i.e. filter the order entries according to the day's date, write all the delivery addresses on a piece of paper, map their route, and go about the deliveries.
+
+This app hopes to automate that process, supposing there is some way to retrieve a JSON file with all the orders from Shopify and link this API so that it makes a request for data every X hours.
+
+### MVP
+A system that lets you:
+1. Log in as admin
+2. View orders
+3. Update orders
+
+### Further
+1. Sort orders by postal code
+2. Map a delivery route for current day's orders
+
+### Further
+1. Retrieve orders from Shopify database
+
+### Further
+1. Include Google Map for visualising delivery route
+
+### How the main page should look:
+![wireframe](public/images/wireframe.PNG)
+
+### Data-storage:
+```js
+User = {
+    name
+    email
+    password
+}
+
+Customer = {
+    name
+    phone
+    address
+    postal
+    orders: [ { item, quantity } ]
+    expectedDelivery: { date, time }
+    status //either unfulfilled, partially-fulfillled or fulfilled
+}
+
+Item = {
+    name
+    stock
+}
+```
+
+
+
---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const expressEjsLayouts = require("express-ejs-layouts");
+
+require("dotenv").config();
+
+//authentication
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+
+//connect mongoDB
+mongoose.connect(
+    process.env.MONGODB,
+    {
+       useCreateIndex: true,
+       useNewUrlParser: true,
+       useUnifiedTopology: true 
+    },
+    () => console.log("MongoDB connected for Haste")
+);
+
+//==================== middleware ====================//
+
+app.use(express.static("public"));
+app.use(express.urlencoded( {extended: true} ));
+app.set("view engine", "ejs");
+app.use(expressEjsLayouts);
+
+//express sessions
+app.use(
+    session ({
+        secret: process.env.SECRET,
+        saveUninitialized: true,
+        resave: false,
+        cookie: { maxAge: 360000 }
+    })
+);
+
+//passport
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use(function(request, response, next) {
+    response.locals.alerts = request.flash();
+    response.locals.currentUser = request.user;
+    next();
+});
+
+//==================== routes ====================//
+
+app.get("/", (req, res) => {
+    res.render("test");
+});
+
+//==================== port listen ====================//
+
+app.listen(process.env.PORT, function() {
+    console.log(`Haste running at localhost:${process.env.PORT}`);
+})
